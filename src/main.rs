c3574e60823bb6176b3232f0d87cbeb0f341f752
Thomas Olsen <tolsen1@wayfair.com>
2021-11-02 14:56:34 -0400

    Service Skeleton

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod services;
+
+use services::test_service;
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::scope("/services").configure(test_service::test_config))
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
